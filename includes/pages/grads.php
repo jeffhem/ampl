9d95c2fc0c449855f4464186222f276fe933b337
Jeff13 <jeffhem@gmail.com>
2015-03-28 11:17:26 -0400

    tracking all files

---
+++ includes/pages/grads.php
@@ -0,0 +1,296 @@
+<h1> Graduate Students</h1>
+
+<!-------- Mina -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#mina">
+		<img src="img/asset/mina.jpg" alt="" style="width:100%;">
+		<h3>Mina Yaghmazadeh</h3>
+		<span>PhD candidate</span>
+
+	</div>
+</div>
+<div class="modal fade" id="mina" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Mina Yaghmazadeh</h1>
+
+				<h3>Education</h3>
+				<p>PhD Candidate - Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>MS, 2009 - Materials Science and Engineering, Materials Selection and Characterization, University of Tehran, Tehran, Iran</p>
+				<p>BS, 2003 - Materials Science and Engineering, University of Tehran, Tehran, Iran</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>Uniform-droplet spray process</li>
+					<li>Transformation kinetics</li>
+					<li>Surface treatments</li>
+					<li>Plasma electrolytic techniques</li>
+					<li>Electrochemical and mechanical behavior of nano-coatings</li>
+					<li>Coating defect evaluation</li>
+				</ul>
+
+				<h3>Publications</h3>
+				<ul>
+					<li><strong>M. Yaghmazadeh</strong>, C. Dehghanian, “Surface Hardening of AISI H13 Steel Using Pulsed Plasma Electrolytic Carburizing (PPEC),” Plasma Processes and Polymers, Supplemental Issue, Vol. 6, 2009.</li>
+					<li><strong>M. Yaghmazadeh</strong>, C. Dehghanian, “Effect of Plasma Electrolytic Carburizing (PPEC) on Wear Behavior of AISI H13 Steel,” 7th Ceramic Congress, Shiraz, Iran, 2009.</li>
+					<li>P. Taheri, C. Dehghanian, S.H. Alavi, G. Ashtari, <strong>M. Yaghmazadeh</strong>, “Investigation of Electrochemical and Abrasion Behaviour of Nitroborided AISI H13 Steel by Plasma Electrolytic Saturation Method,” Iran Surface Coatings Magazine, No. 24, pp. 23-26, 2008.</li>		
+					<li><strong>M. Yaghmazadeh</strong>, C. Dehghanian, “Surface Hardening of AISI H13 Steel Using Pulsed Plasma Electrolytic Carburizing (PPEC),” 11th International Conference on Plasma Surface Engineering PSE 2008, Garmisch-Partenkirchen, Germany, 2008.</li>		
+					<li>S.H. Alavi, C. Dehghanian, <strong>M. Yaghmazadeh</strong>, G. Ashtari, “Investigation of Some Parameter`s Effect on Corrosion of Borided H13 Steel,” 9th Conference on Surface and Heat Treatments, Iran Science and Technology University, Iran, 2008.</li>
+					<li>C.Dehghanian, <strong>M. Yaghmazadeh</strong>,  G. Ashtari, H. Alavi and P. Taheri, “A Novel Method for Preparing Diffusion Nanocrystaline Coatings on H13 Steel by Plasma Electrolytic Saturation,” 1st New Materials National Congress, Materials & Energy Research Cener (MERC) , Iran, 2008.</li>
+				</ul>
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- Mina -------->
+
+<!-------- Somayeh -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#somayeh">
+		<img src="img/asset/somayeh.jpg" alt="" style="width:100%;">
+		<h3>Somayeh Gheybi</h3>
+		<span>PhD candidate</span>
+
+	</div>
+</div>
+<div class="modal fade" id="somayeh" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Mina Yaghmazadeh</h1>
+
+				<h3>Education</h3>
+				<p>PhD Candidate - Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>MS, 2009 - Applied Mechanical Engineering, Ferdowsi University of Mashhad, Iran</p>
+				<p>BS, 2005 - Mechanical Engineering, Ferdowsi University of Mashhad, Iran</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li> Ultrasonic powder consolidation</li>
+					<li>Composite materials</li>
+					<li>Vibrations and dynamic analysis</li>
+				</ul>
+
+				<h3>Publications</h3>
+				<ul>
+					<li><strong>S. Gheybi Hashemabad</strong>, T. Ando “Continuous-Heating Ignition Testing of Hybrid Al-Ni-CuO Reactive Composites Fabricated by Ultrasonic Powder Consolidation,” TMS 2015 Conference Proceedings - Novel Synthesis and Consolidation of Powder Materials. (in press) </li>
+
+					<li><strong>S. Gheybi Hashemabad</strong>, T. Ando “Ignition Characteristics of Hybrid Al-Ni-Fe2O3 and Al-Ni-CuO Reactive Composites Fabricated by Ultrasonic Powder,” Combustion and Flame, 2014.</li>
+
+					<li>M. Tahani, <strong>S. Gheybi</strong>, “Analysis of Dynamic Interlaminar Stresses in Antisymmetric Angle-ply Composite Laminated Plates,”  3rd International Conference on Advanced Computational Engineering and Experimenting ACE-X, 2009, Rome, Italy, pp. 91-92.</li>
+
+					<li>M. Tahani, <strong>S. Gheybi</strong>, “Analysis of Interlaminar Stresses in General Cross-Ply Composite Laminates Subjected to Transient Vibration,” 7th International Conference on Composite Science and Technology (ICCST), 2008, Sharjeh, United Arab Emirates.</li>
+
+					<li>M. Tahani, <strong>S. Gheybi</strong>, “Analysis of Interlaminar Stresses in General Cross-Ply Composite Plates Subjected to Free Vibration,” 1st International Conference on Composites: Characterization, Fabrication, and Application (CCFA), 2008, Kish, Iran.</li>
+
+					<li>J. Rezaeepazhand, <strong>S. Gheybi</strong>, “Free Vibrations of Delaminated Composite Beam,” (Persian), The 16th International Conference on Mechanical Engineering (ISME), 2008, Kerman, Iran.</li>
+
+					<li>M. Tahani, <strong>S. Gheybi</strong>, “Determination of Proper Position of Steering Wheel Ball Joint of Rack and Pinion Steering System,” (Persian), The 16th International Conference on Mechanical Engineering (ISME), 2008, Kerman, Iran.</li>
+
+					<li>M. Tahani, <strong>S. Gheybi</strong>, “Analysis of Interlaminar Stresses in General cross-ply composite plates with simply supported edges subjected to free vibration,” (Persian), National Congress on Manufacturing Engineering (NMEC), 2008, Najafabad, Iran.</li>
+				</ul>
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- Somayeh -------->
+
+<!-------- Nazanin -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#nazanin">
+		<img src="img/asset/nazanin.jpg" alt="" style="width:100%;">
+		<h3>Nazanin Mokarram</h3>
+		<span>PhD candidate</span>
+
+	</div>
+</div>
+<div class="modal fade" id="nazanin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Nazanin Mokarram</h1>
+
+				<h3>Education</h3>
+				<p>PhD Candidate - Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>MS, 2009 - Mechanical Engineering, University of California, Irvine</p>
+				<p>BS, 2007 - Mechanical Engineering, Sharif University of Technology, Tehran, Iran</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>Ultrasonic powder consolidation</li>
+					<li>Metallurgical characterization</li>
+					<li>Solid mechanics</li>
+					<li>Biomechanics</li>
+				</ul>
+				
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- Nazanin -------->
+
+<!-------- Tianyu -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#tianyu">
+		<img src="img/asset/tianyu.jpg" alt="" style="width:100%;">
+		<h3>Tianyu Hu</h3>
+		<span>PhD student</span>
+
+	</div>
+</div>
+<div class="modal fade" id="tianyu" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Tianyu Hu</h1>
+
+				<h3>Education</h3>
+				<p>PhD Student – Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>BS, 2007-2011-Mechanical Engineering, Shanghai Maritime University, Shanghai, China</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>-Ultrasonic  Materials  Processing</li>
+					<li>Enhanced Diffusion by Ultrasonic Deformation</li>
+					<li>Metallurgical Characterization</li>
+				</ul>
+				<h3>Publications</h3>
+				<ul>
+					<li><strong>T. Hu</strong>, S. Zhalehpour, A. Gouldstone, S. Muftu and T. Ando, “A Method for the Estimation of the Interface Temperature in Ultrasonic Joining,” Metall. Mater. Trans. A, Vol. 45A, 2014, pp. 2545-2552.</li>
+				</ul>
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- Tianyu -------->
+
+<!-------- yangfan -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#yangfan">
+		<img src="img/asset/yangfan.jpg" alt="" style="width:100%;">
+		<h3>Yangfan Li</h3>
+		<span>PhD student</span>
+
+	</div>
+</div>
+<div class="modal fade" id="yangfan" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Yangfan Li</h1>
+
+				<h3>Education</h3>
+				<p>PhD Student – Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>BS, 2010 – Material Science and Engineering, South China University of Technology, Guangzhou, China</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>Ultrasonic powder consolidation</li>
+					<li>Composite Materials</li>
+				</ul>
+				
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- yangfan -------->
+
+<!-------- azin -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#azin">
+		<img src="img/asset/azin.jpg" alt="" style="width:100%;">
+		<h3>Azin Houshmand</h3>
+		<span>PhD student</span>
+
+	</div>
+</div>
+<div class="modal fade" id="azin" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Azin Houshmand</h1>
+
+				<h3>Education</h3>
+				<p>PhD Student – Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>BS, 2012 – Materials Science and Engineering, Sharif University of Technology, Tehran, Iran</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>Phase Transformation</li>
+					<li>Solidification process</li>
+				</ul>
+				
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- azin -------->
+
+<!-------- zheng -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#zheng">
+		<img src="img/asset/zheng.jpg" alt="" style="width:100%;">
+		<h3>Zheng Liu</h3>
+		<span>MS student</span>
+
+	</div>
+</div>
+<div class="modal fade" id="zheng" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Zheng Liu</h1>
+
+				<h3>Education</h3>
+				<p>MS Student – Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>BS, 2011- Mechanical Engineering, Changchun University of Science and Technology, Changchun, China.</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>Ultrasonic powder consolidation</li>
+				</ul>
+				
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- zheng -------->
+
+<!-------- guangsai -------->
+<div class="col-md-3">
+	<div class="profileGrid" data-toggle="modal" data-target="#guangsai">
+		<img src="img/asset/guangsai.jpg" alt="" style="width:100%;">
+		<h3>Guangsai Wei</h3>
+		<span>MS student</span>
+
+	</div>
+</div>
+<div class="modal fade" id="guangsai" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+    		<div class="modal-body">
+	    		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h1>Guangsai Wei</h1>
+
+				<h3>Education</h3>
+				<p>MS Student – Mechanical Engineering, Northeastern University, Boston, MA</p>
+				<p>BS, 2010- Metal Material Engineering, Sichuan University, Chengdu, China.</p>
+
+				<h3>Research Interests</h3>
+				<ul>
+					<li>Uniform-droplet spray</li>
+				</ul>
+				
+			</div>
+		</div>				
+	</div>
+</div>
+<!-------- guangsai -------->
---
+++ includes/pages/prof.php
@@ -0,0 +1,52 @@
+<div class="col-md-4">
+	<div id="profile">
+		<img src="img/asset/prof-taiichi.jpg" alt="" style="width:100%;">
+		<div id="profileDetail">
+			<h2>Prof. Teiichi Ando</h2>
+			<address>
+				<strong>Phone:</strong> 1231231234<br>
+				<strong>Email:</strong> <a href="mailto:tando@coe.neu.edu" target="_blank">tando@coe.neu.edu</a><br>
+			</address>
+			<quote>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consequat, augue sed facilisis convallis, ex ante mollis odio, id hendrerit est purus in magna.”
+			</quote>
+		</div>
+	</div>
+</div>
+<div class="col-md-8">
+	<div id="bio">
+		<h1>Prof. Teiichi Ando</h1>
+
+		<h3>Education</h3>
+		<p>PhD Metallurgy, Colorado School of Mines, Golden, CO, USA, 1982</p>
+		<p>MS Metallurgy, Colorado School of Mines, Golden, CO, USA, 1979</p>
+		<p>BS Metallurgy, Kyoto University, Kyoto, Japan, 1971</p>
+
+		<h3>Research Interests</h3>
+		<ul>
+			<li>Rapid solidification processing</li>
+			<li>Non-equilibrium processing</li>
+			<li>Powder metallurgy</li>
+			<li>Processing-structure-property relationship in materials</li>
+			<li>Materials processing for advanced manufacturing</li>
+			<li>Ultrasonic processing of materials</li>
+		</ul>
+		
+		<h3>Teaching Areas</h3>
+		<h4>Undergraduate Courses</h4>
+		<ul>
+			<li>Materials Science</li>
+			<li>Materials Processing</li>
+		</ul>
+
+		<h4>Graduate Courses</h4>
+		<ul>
+			<li>MATL7355 Thermodynamics of Materials</li>
+			<li>MATL7360 Kinetics of Phase Transformations</li>
+			<li>MATL7350 Mechanical Behavior and Strengthening Mechanisms</li>
+			<li>MATL7395 Fundamentals of Solidification</li>
+		</ul>
+
+		<h3>Publications</h3>
+		<p>Please click <a href="Publications">here</a> to see the list of selected publications.</p>
+	</div>
+</div>
